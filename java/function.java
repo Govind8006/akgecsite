65949f0032df78b8ac872eb54736ec02586ac839
Govind <govindaggarwal8006@gmail.com>
2022-09-04 12:16:36 +0530

    first commit

---
+++ java/function.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class function {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        printMyName(a, b);
+    }
+
+    public static void printMyName(int a, int b) {
+
+        System.out.println(a + b);
+    }
+}
